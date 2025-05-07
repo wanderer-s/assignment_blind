ea32aa23fbde64ec1b28d851edc01477f7d7cac5
wanderer-s <ows0871@gmail.com>
2025-05-08 03:43:28 +0900

    swagger 적용, validation pipe 적용

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { setSwaggerConfig } from './config/swagger.config';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  setSwaggerConfig(app);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
