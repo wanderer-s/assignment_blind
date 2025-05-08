018c9bbd8667a999544dacd523113709541392cc
wanderer-s <ows0871@gmail.com>
2025-05-09 02:17:34 +0900

    Comment module import

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { PostModule } from './post/post.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { getTypeORMConfig } from './config/typeorm.config';
+import { CommentModule } from './comment/comment.module';
+import { RouterModule } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -17,6 +19,13 @@ import { getTypeORMConfig } from './config/typeorm.config';
       },
     }),
     PostModule,
+    CommentModule,
+    RouterModule.register([
+      {
+        path: 'post/:postId',
+        module: CommentModule,
+      },
+    ]),
   ],
 })
 export class AppModule {}
