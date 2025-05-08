ab7c46d51457714b8ec8df17c79d4701e4269c41
wanderer-s <ows0871@gmail.com>
2025-05-08 13:36:17 +0900

    post module, config module, type orm module 처리

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,22 @@
 import { Module } from '@nestjs/common';
+import { PostModule } from './post/post.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getTypeORMConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return getTypeORMConfig(configService);
+      },
+    }),
+    PostModule,
+  ],
 })
 export class AppModule {}
